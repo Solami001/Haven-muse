bba757bb050e72d5bfa99fc73b2eee402c3eec02
Solami001 <akindoyin800@gmail.com>
2024-06-24 00:41:48 +0100

    update

--- script.js
+++ script.js
@@ -12,32 +12,54 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
-document.getElementById('customForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+// document.getElementById('customForm').addEventListener('submit', function(event) {
+//     event.preventDefault();
 
-    var firstName = document.getElementById('firstName').value;
-    var lastName = document.getElementById('lastName').value;
-    var emailAddress = document.getElementById('emailAddress').value;
-    var message = document.getElementById('message').value;
+//     var firstName = document.getElementById('firstName').value;
+//     var lastName = document.getElementById('lastName').value;
+//     var emailAddress = document.getElementById('emailAddress').value;
+//     var message = document.getElementById('message').value;
 
-    fetch('https://script.google.com/macros/s/AKfycbxtk3n_ZF7cWfk7uvNOtTG7jPiJfZiyoeBq9hSNwJifIy0b6-kYet5-oaSGpfyGU5HE/exec', {
-        method: 'POST',
-        body: JSON.stringify({FirstName: firstName, lastName: lastName, emailAddress: emailAddress, message: message}),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.result === 'success') {
-            document.getElementById('responseMessage').innerText = 'Form submitted successfully!';
-            document.getElementById('customForm').reset();
+//     fetch('https://script.google.com/macros/s/AKfycbxtk3n_ZF7cWfk7uvNOtTG7jPiJfZiyoeBq9hSNwJifIy0b6-kYet5-oaSGpfyGU5HE/exec', {
+//         method: 'POST',
+//         body: JSON.stringify({FirstName: firstName, lastName: lastName, emailAddress: emailAddress, message: message}),
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         if (data.result === 'success') {
+//             document.getElementById('responseMessage').innerText = 'Form submitted successfully!';
+//             document.getElementById('customForm').reset();
+//         } else {
+//             document.getElementById('responseMessage').innerText = 'There was an error submitting the form.';
+//         }
+//     })
+//     .catch(error => {
+//         document.getElementById('responseMessage').innerText = 'There was an error submitting the form.';
+//         console.error('Error:', error);
+//     });
+// });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navbarIcon = document.getElementById('navbar-icon');
+    const menuIcon = document.getElementById('menu-icon');
+    const cancelIcon = document.getElementById('cancel-icon');
+    const sidebar = document.getElementById('sidebar');
+
+    navbarIcon.addEventListener('click', function() {
+        menuIcon.classList.toggle('hidden');
+        cancelIcon.classList.toggle('hidden');
+        sidebar.classList.toggle('hidden-sidebar');
+
+        if (menuIcon.classList.contains('hidden')) {
+            menuIcon.classList.add('animate-spin');
         } else {
-            document.getElementById('responseMessage').innerText = 'There was an error submitting the form.';
+            menuIcon.classList.remove('animate-spin');
         }
-    })
-    .catch(error => {
-        document.getElementById('responseMessage').innerText = 'There was an error submitting the form.';
-        console.error('Error:', error);
     });
 });
+
+  
